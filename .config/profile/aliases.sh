dbde2580bf01c46ba68b192d11ba9dd7f8c39773
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-03-20 15:24:18 +1300

    Clean up aliases

--- .config/profile/aliases.sh
+++ .config/profile/aliases.sh
@@ -1,7 +1,14 @@
+# Dotfiles
 alias dotfiles='/usr/bin/git --git-dir="${HOME}/etc/" --work-tree="${HOME}"'
-alias fzb='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
-alias ls='ls --color=auto -F'
-alias ll='ls -l'
-alias la='ll -A'
+
+# Colours
+alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 
+# Standard
+alias ls='ls -F'
+alias ll='ls -l'
+alias la='ll -A'
+
+# Fuzz
+alias fvim='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}" --bind "enter:become(vim {})"'
