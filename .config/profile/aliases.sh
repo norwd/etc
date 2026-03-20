5f570402b739a91ccc2b94db17d97501fd53ae51
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-03-20 14:48:09 +1300

    Add bat/fzf alias
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/profile/aliases.sh
+++ .config/profile/aliases.sh
@@ -1,4 +1,5 @@
 alias dotfiles='/usr/bin/git --git-dir="${HOME}/etc/" --work-tree="${HOME}"'
+alias fzb='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
 alias ls='ls --color=auto -F'
 alias ll='ls -l'
 alias la='ll -A'
