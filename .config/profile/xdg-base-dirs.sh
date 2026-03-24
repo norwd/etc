b424240e11923a797263de3f0e4110e0d9124fbf
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-03-25 12:53:15 +1300

    Remove `mkdir`, these are `.gitkeep`ed now

--- .config/profile/xdg-base-dirs.sh
+++ .config/profile/xdg-base-dirs.sh
@@ -9,10 +9,3 @@ export XDG_DATA_HOME="$HOME/.local/share"
 
 # Where user-specific state files should be written (analogous to /var/lib).
 export XDG_STATE_HOME="$HOME/.local/state"
-
-# Ensure these directories exist
-[ ! -d "${XGD_CONFIG_HOME}" ] && mkdir -p "${XDG_CONFIG_HOME}"
-[ ! -d "${XGD_CACHE_HOME}" ] && mkdir -p "${XDG_CACHE_HOME}"
-[ ! -d "${XGD_DATA_HOME}" ] && mkdir -p "${XDG_DATA_HOME}"
-[ ! -d "${XGD_STATE_HOME}" ] && mkdir -p "${XDG_STATE_HOME}"
-
