47b8d27c9ef90d287734411708bf7076acf98156
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-02 14:28:11 +1300

    Fix whitespace

--- .config/zsh/070-autosuggestions.zsh
+++ .config/zsh/070-autosuggestions.zsh
@@ -4,13 +4,13 @@ export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 # shellcheck disable=SC2154
 for _zsh_autosuggestions_path in \
-  "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" \
-  "${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
+	"${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" \
+	"${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
 do
-  if [ -r "${_zsh_autosuggestions_path}" ]
-  then
-    # shellcheck source=/dev/null
-    source "${_zsh_autosuggestions_path}"
-    break
-  fi
+	if [ -r "${_zsh_autosuggestions_path}" ]
+	then
+		# shellcheck source=/dev/null
+		source "${_zsh_autosuggestions_path}"
+		break
+	fi
 done
