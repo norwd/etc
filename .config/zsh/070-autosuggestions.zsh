3e3f3ee86e7440ddf0726889c9e56eaf44814d52
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-02 14:08:38 +1300

    Disable SC2154

--- .config/zsh/070-autosuggestions.zsh
+++ .config/zsh/070-autosuggestions.zsh
@@ -2,6 +2,7 @@
 
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
+# shellcheck disable=SC2154
 for _zsh_autosuggestions_path in \
   "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" \
   "${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
