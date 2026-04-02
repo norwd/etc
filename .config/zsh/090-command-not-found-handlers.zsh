6cc67a032c8fcb916859c3f77b86b9571daf6a40
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-02 13:55:07 +1300

    De-tabify

--- .config/zsh/090-command-not-found-handlers.zsh
+++ .config/zsh/090-command-not-found-handlers.zsh
@@ -2,48 +2,48 @@
 
 if [ -x "$(command -v pkgfile)" ]
 then
-	# shellcheck source=/dev/null
-	source /usr/share/doc/pkgfile/command-not-found.zsh
+  # shellcheck source=/dev/null
+  source /usr/share/doc/pkgfile/command-not-found.zsh
 elif [ -x "$(command -v apt-get)" ]
 then
-	command_not_found_handler() {
-		local cmd="$1"
+  command_not_found_handler() {
+    local cmd="$1"
 
-		if apt-get install --dry-run "${cmd}" 1>/dev/null 2>&1
-		then
-			if read -r -q "?zsh: command not found, install ${cmd} with apt-get [nyae]? "
-			then
-				printf '\nzsh: installing %s...\n' "${cmd}"
-				$([[ $(id -u) == 0 ]] || echo sudo) apt-get install "${cmd}"
-				return 0
-			else
-				printf '\nzsh: not installing %s\n' "${cmd}"
-			fi
-		else
-			printf 'zsh: command not found: %s\n' "${cmd}" 1>&2
-		fi
+    if apt-get install --dry-run "${cmd}" 1>/dev/null 2>&1
+    then
+      if read -r -q "?zsh: command not found, install ${cmd} with apt-get [nyae]? "
+      then
+        printf '\nzsh: installing %s...\n' "${cmd}"
+        $([[ $(id -u) == 0 ]] || echo sudo) apt-get install "${cmd}"
+        return 0
+      else
+        printf '\nzsh: not installing %s\n' "${cmd}"
+      fi
+    else
+      printf 'zsh: command not found: %s\n' "${cmd}" 1>&2
+    fi
 
-		return 127
-	}
+    return 127
+  }
 elif [ -x "$(command -v brew)" ]
 then
-	command_not_found_handler() {
-		local cmd="$1"
+  command_not_found_handler() {
+    local cmd="$1"
 
-		if brew install --dry-run "${cmd}" 1>/dev/null 2>&1
-		then
-			if read -r -q "?zsh: command not found, install ${cmd} with homebrew [nyae]? "
-			then
-				printf '\nzsh: installing %s...\n' "${cmd}"
-				brew install "${cmd}"
-				return 0
-			else
-				printf '\nzsh: not installing %s\n' "${cmd}"
-			fi
-		else
-			printf 'zsh: command not found: %s\n' "${cmd}" 1>&2
-		fi
+    if brew install --dry-run "${cmd}" 1>/dev/null 2>&1
+    then
+      if read -r -q "?zsh: command not found, install ${cmd} with homebrew [nyae]? "
+      then
+        printf '\nzsh: installing %s...\n' "${cmd}"
+        brew install "${cmd}"
+        return 0
+      else
+        printf '\nzsh: not installing %s\n' "${cmd}"
+      fi
+    else
+      printf 'zsh: command not found: %s\n' "${cmd}" 1>&2
+    fi
 
-		return 127
-	}
+    return 127
+  }
 fi
