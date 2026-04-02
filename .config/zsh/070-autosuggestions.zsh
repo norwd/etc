a94cca0c2618fc62901b761a4346995a267f265a
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-02 13:55:19 +1300

    Fix some zsh shellchecks

--- .config/zsh/070-autosuggestions.zsh
+++ .config/zsh/070-autosuggestions.zsh
@@ -4,11 +4,12 @@ export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 for _zsh_autosuggestions_path in \
   "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" \
-  "${XDG_DATA_HOME}/zsh/autosuggestions/zsh-autosuggestions.zsh"
+  "${XDG_DATA_HOME:-$HOME/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
 do
-  if [ -r "$_zsh_autosuggestions_path" ]
+  if [ -r "${_zsh_autosuggestions_path}" ]
   then
-    source "$_zsh_autosuggestions_path"
+    # shellcheck source=/dev/null
+    source "${_zsh_autosuggestions_path}"
     break
   fi
 done
