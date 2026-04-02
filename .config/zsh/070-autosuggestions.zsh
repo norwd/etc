77cddddc8c5d4f4f54d1976ae703f6c277898b57
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-02 16:00:03 +1300

    Fix ZC1010

--- .config/zsh/070-autosuggestions.zsh
+++ .config/zsh/070-autosuggestions.zsh
@@ -5,7 +5,7 @@ export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 # shellcheck disable=SC2154
 for _zsh_autosuggestions_path in "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" "${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
 do
-	if [ -r "${_zsh_autosuggestions_path}" ]
+	if [[ -r "${_zsh_autosuggestions_path}" ]]
 	then
 		# shellcheck source=/dev/null
 		source "${_zsh_autosuggestions_path}"
