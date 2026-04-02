4b5bf40f99e512089cb7a28e16d3b2ef3a0f1671
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-02 15:58:45 +1300

    Unwrap for loop

--- .config/zsh/070-autosuggestions.zsh
+++ .config/zsh/070-autosuggestions.zsh
@@ -3,9 +3,7 @@
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 # shellcheck disable=SC2154
-for _zsh_autosuggestions_path in \
-	"${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" \
-	"${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
+for _zsh_autosuggestions_path in "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" "${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
 do
 	if [ -r "${_zsh_autosuggestions_path}" ]
 	then
