c141dc60cd18b31db8d0b04d484b9300820b9e9c
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-13 14:02:48 +1200

    Fix 050-gempaths.sh

--- .config/profile.d/050-gempaths.sh
+++ .config/profile.d/050-gempaths.sh
@@ -3,7 +3,6 @@
 
 if [ -x "$(command -v ruby)" ] && [ -x "$(command -v gem)" ]
 then
-  # shellcheck disable=SC2155
-  export PATH="${PATH}:$(ruby -r rubygems -e 'puts Gem.user_dir')/bin"
+	# shellcheck disable=SC2155
+	export PATH="${PATH}:$(ruby -r rubygems -e 'puts Gem.user_dir')/bin"
 fi
-
