4e83331011606898a82161f9735964466db7c97b
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-15 14:15:39 +1200

    Remove redundant shellcheck directive

--- .config/zsh/020-autosuggestions.zsh
+++ .config/zsh/020-autosuggestions.zsh
@@ -2,7 +2,6 @@
 
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
-# shellcheck disable=SC2154
 for _zsh_autosuggestions_path in "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" "${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
 do
 	if [[ -r "${_zsh_autosuggestions_path}" ]]
