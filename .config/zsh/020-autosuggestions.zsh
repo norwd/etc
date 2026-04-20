c9f30c16cf78cee91ccda75025abcff2ba5519dc
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-21 09:41:22 +1200

    Fix single bracket tests

--- .config/zsh/020-autosuggestions.zsh
+++ .config/zsh/020-autosuggestions.zsh
@@ -4,7 +4,7 @@ export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 for _zsh_autosuggestions_path in "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" "${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/autosuggestions/zsh-autosuggestions.zsh"
 do
-	if [ -r "${_zsh_autosuggestions_path}" ]
+	if [[ -r "${_zsh_autosuggestions_path}" ]]
 	then
 		# shellcheck source=/dev/null
 		source "${_zsh_autosuggestions_path}"
