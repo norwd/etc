82b266c6edc30d6849cd85217a616c91715291f8
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-23 17:14:46 +1200

    Disable ZC1104

--- .config/profile.d/015-xdg-extra-dirs.sh
+++ .config/profile.d/015-xdg-extra-dirs.sh
@@ -3,4 +3,4 @@
 # Where user-specific executable files should be written (analogous to /usr/bin) [NON-STANDARD].
 export XDG_BIN_HOME="${HOME}/.local/bin"
 
-export PATH="${PATH}:${XDG_BIN_HOME}" # zshellcheck disable=ZC1188
+export PATH="${PATH}:${XDG_BIN_HOME}" # zshellcheck disable=ZC1104,ZC1188
