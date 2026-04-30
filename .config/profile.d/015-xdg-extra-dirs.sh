94438f41cee1f20e6051dbe2e2b2a54ef3e97f61
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-04-30 13:42:35 +1200

    Avoid xdg hardcoding (#69)

--- .config/profile.d/015-xdg-extra-dirs.sh
+++ .config/profile.d/015-xdg-extra-dirs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Where user-specific executable files should be written (analogous to /usr/bin) [NON-STANDARD].
-export XDG_BIN_HOME="${HOME}/.local/bin"
+export XDG_BIN_HOME="${XDG_BIN_HOME:-${HOME}/.local/bin}"
 
 export PATH="${PATH}:${XDG_BIN_HOME}" # zshellcheck disable=ZC1104,ZC1188
