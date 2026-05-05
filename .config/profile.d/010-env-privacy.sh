e604ceb80c98d020c925abf62190182e455b5902
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-06 11:12:17 +1200

    Create 010-env-privacy.sh

---
+++ .config/profile.d/010-env-privacy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export DO_NOT_TRACK="1"
+export GH_TELEMETRY="0" # this is the inverse of the others, `1` enables telemetry
+export DISABLE_TELEMETRY="1"
+export HOMEBREW_NO_ANALYTICS=1
