9df900eda0734d1aaa969a2f7827f5deb097d5ef
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-13 16:42:43 +1200

    Point `XDG_PROJECTS_DIR` to my existing `~/src` directory (#93)
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/profile.d/015-xdg-user-dirs.sh
+++ .config/profile.d/015-xdg-user-dirs.sh
@@ -5,5 +5,5 @@ export XDG_MUSIC_DIR="${HOME}/Music"
 export XDG_PICTURES_DIR="${HOME}/Pictures"
 export XDG_VIDEOS_DIR="${HOME}/Movies" # MacOS doesn't like ~/Videos
 export XDG_PUBLICSHARE_DIR="${HOME}/Public"
-export XDG_PROJECTS_DIR="${XDG_DOCUMENTS_DIR}/Projects" # Low usage, put it under documents for tidyness
+export XDG_PROJECTS_DIR="${HOME}/src/norwd" # All my git clones go in ~/src/<user>/<repo>
 export XDG_TEMPLATES_DIR="${XDG_DOCUMENTS_DIR}/Templates" # Low usage, put it under documents for tidyness
