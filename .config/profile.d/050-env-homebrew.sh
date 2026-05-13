f58b258a0fa0b9f7b1ee40cac1a81696476907aa
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-13 20:19:50 +1200

    Create temp file for homebrew shell env
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/profile.d/050-env-homebrew.sh
+++ .config/profile.d/050-env-homebrew.sh
@@ -2,7 +2,14 @@
 
 if [ -x "$(command -v brew)" ]
 then
-	eval "$(brew shellenv)" # zshellcheck disable=ZC1046,ZC1098
+	_brew_shellenv_cache_file="${XDG_CONFIG_HOME:-${HOME}/.config}/profile.d/051-env-homebrew-shellenv-cache.sh"
+
+	if [ ! -f "${_brew_shellenv_cache_file}" ] || [ -n "$(find "${XDG_CONFIG_HOME:-${HOME}/.config}" -name "$(basename "${_brew_shellenv_cache_file}")" -mtime +1)" ]
+	then
+		brew shellenv > "${_brew_shellenv_cache_file}"
+	fi
+
+	. "${_brew_shellenv_cache_file}"
 
 	if [ -x "$(command -v bat)" ]
 	then
