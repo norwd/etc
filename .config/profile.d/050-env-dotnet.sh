878ccdecae63b730548d811e9f05aeefe4d748f3
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-21 13:19:13 +1200

    Set `DOTNET_ROOT` and add to path if exists
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/profile.d/050-env-dotnet.sh
+++ .config/profile.d/050-env-dotnet.sh
@@ -1,3 +1,8 @@
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
 export DOTNET_NOLOGO=true
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+export DOTNET_ROOT="${HOME}/.dotnet"
+if [ -d "${DOTNET_ROOT}" ]
+then
+	export PATH="${PATH}:${DOTNET_ROOT}"
+fi
