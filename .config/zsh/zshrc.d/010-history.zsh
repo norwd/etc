f956dc157214303241d4679f90ba9f3e13d81aa3
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-06-11 12:39:50 +1200

    Update 010-history.zsh
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/zsh/zshrc.d/010-history.zsh
+++ .config/zsh/zshrc.d/010-history.zsh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+#
+# Copyright (C) 2026 Y. Meyer-Norwood
+# This file is distributed under the Hippocratic License 3.0 (HL3-CL-ECO-LAW-MIL-SV)
+#
+
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
 
