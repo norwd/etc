a35480fbbf81f07f1032bb36316e332337e1d4d7
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-06-11 12:44:19 +1200

    Update 015-xdg-extra-dirs.sh
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/profile.d/015-xdg-extra-dirs.sh
+++ .config/profile.d/015-xdg-extra-dirs.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#
+# Copyright (C) 2026 Y. Meyer-Norwood
+# This file is distributed under the Hippocratic License 3.0 (HL3-CL-ECO-LAW-MIL-SV)
+#
+
 # Where user-specific executable files should be written (analogous to /usr/bin) [NON-STANDARD].
 export XDG_BIN_HOME="${XDG_BIN_HOME:-${HOME}/.local/bin}"
 export PATH="${PATH}:${XDG_BIN_HOME}"
