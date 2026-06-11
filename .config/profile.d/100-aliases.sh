8eaf5eb7b6b43138808e6771c45ce17c32b29017
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-06-11 12:42:43 +1200

    Update 100-aliases.sh
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/profile.d/100-aliases.sh
+++ .config/profile.d/100-aliases.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#
+# Copyright (C) 2026 Y. Meyer-Norwood
+# This file is distributed under the Hippocratic License 3.0 (HL3-CL-ECO-LAW-MIL-SV)
+#
+
 # Dotfiles
 alias dotfiles='/usr/bin/git --git-dir="${HOME}/etc/" --work-tree="${HOME}"'
 
