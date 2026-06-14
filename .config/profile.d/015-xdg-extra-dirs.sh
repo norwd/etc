71d08fa62e13230322144a336d6365f78818aebb
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-06-15 10:53:21 +1200

    Use `LicenseRef-` syntax to make Hippocratic license vaild for REUSE spec
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/profile.d/015-xdg-extra-dirs.sh
+++ .config/profile.d/015-xdg-extra-dirs.sh
@@ -3,7 +3,7 @@
 #
 # SPDX-FileCopyrightText: 2026 Y. Meyer-Norwood <norwd@noreply.codeberg.org>
 # SPDX-FileCopyrightText: 2026 Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
-# SPDX-License-Identifier: Hippocratic-3.0-CL-ECO-LAW-MIL-SV
+# SPDX-License-Identifier: LicenseRef-Hippocratic-3.0-CL-ECO-LAW-MIL-SV
 #
 
 # Where user-specific executable files should be written (analogous to /usr/bin) [NON-STANDARD].
