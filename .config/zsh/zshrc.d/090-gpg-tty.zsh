7fad5b3c0b0226c893b41390f440d325a7cf836c
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-06-14 22:29:56 +1200

    Update 090-gpg-tty.zsh
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- .config/zsh/zshrc.d/090-gpg-tty.zsh
+++ .config/zsh/zshrc.d/090-gpg-tty.zsh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
 #
+# SPDX-FileCopyrightText: 2026 Y. Meyer-Norwood <norwd@noreply.codeberg.org>
 # SPDX-FileCopyrightText: 2026 Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
-# SPDX-License-Identifier: HL3-CL-ECO-LAW-MIL-SV
+# SPDX-License-Identifier: Hippocratic-3.0-CL-ECO-LAW-MIL-SV.txt
 #
 
 # shellcheck disable=SC2155
